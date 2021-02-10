3801cc60c58d149d8768e30c47301df580fb0c5a
Narastro <advice02@naver.com>
2021-02-10 11:47:59 +0900

    Align Coordinates

---
+++ Algorithms(Baekjoon)/Q 11650.py
@@ -0,0 +1,12 @@
+# 2021.02.10. Baekjoon algorithm problem #11650
+# Align Coordinates
+
+import sys
+
+N=int(sys.stdin.readline())
+cord=list()
+for i in range(N):
+    cord.append(sys.stdin.readline().split())
+cord.sort(key=lambda x:(int(x[0]),int(x[1])))
+for i in range(N):
+    print(*cord[i])
