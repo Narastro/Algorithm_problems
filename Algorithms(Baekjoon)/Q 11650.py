4f9bf4898869785a673313b155718fbb761698a5
Narastro <advice02@naver.com>
2021-02-10 11:48:53 +0900

    Align Coordinates

--- Algorithms(Baekjoon)/Q 11650.py
+++ Algorithms(Baekjoon)/Q 11650.py
@@ -5,8 +5,11 @@ import sys
 
 N=int(sys.stdin.readline())
 cord=list()
+
 for i in range(N):
     cord.append(sys.stdin.readline().split())
-cord.sort(key=lambda x:(int(x[0]),int(x[1])))
+
+cord.sort(key=lambda x:(int(x[0]),int(x[1])))       # sort by x and y coordinates
+
 for i in range(N):
     print(*cord[i])
