d1f8e7bffcca2368c420adb38c7a20eef0ad7aca
Narastro <78056880+Narastro@users.noreply.github.com>
2021-02-15 18:07:24 +0900

    Start and link ver.2

--- Algorithms(Baekjoon)/Q 14889.py
+++ Algorithms(Baekjoon)/Q 14889.py
@@ -1,12 +1,13 @@
 # 2021.02.15. Baekjoon algorithm problem #14889
 # Start and link
 
+from itertools import combinations
+
 # input
 N=int(input())
 chart=[list(map(int, input().split())) for _ in range(N)]
 
-flag=[False]*(N+1)
-
+# calculation score
 def score(x:list[int])->int:
     val=0
     for i in x:
@@ -16,28 +17,20 @@ def score(x:list[int])->int:
             val+=chart[i-1][j-1]
     return val
 
+# number of possible team cases
+possible_team=[]
+member=[i for i in range(1,N+1)] # member is 1~N
+for team in list(combinations(member,N//2)):        # using itertools.combinations
+    possible_team.append(team)
 
-def Num_case(x,depth:int,N:int):
-    global start
+# calculation minimum score
+min_value=10000
+for i in range(len(possible_team)//2):
+    start=possible_team[i]
+    link=possible_team[-i-1]
+    min_value=min(min_value,abs(score(start)-score(link)))
 
-    if depth==N/2-1:
-        start.append(x)
-    else:
-        for i in range(2,N+1):
-            if not flag[i]:
-                flag[i]=True
-                Num_case(x+[i],depth+1,N)
-                flag[i]=False
-start=[]
-Num_case([1],0,N)
-A=set([i for i in range(1,N+1)])
-min=1000
-for i in range(len(start)):
-    link=A-set(start[i])
-    if abs(score(list(link))-score(start[i]))<min:
-        min=abs(score(list(link))-score(start[i]))
-
-print(min)
+print(min_value)
 
 
 
