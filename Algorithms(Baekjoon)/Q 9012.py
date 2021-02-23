2d1e5be799991f7d86b00facff5c85fb794e10f6
Narastro <78056880+Narastro@users.noreply.github.com>
2021-02-23 16:08:01 +0900

    Parenthesis

--- Algorithms(Baekjoon)/Q 9012.py
+++ Algorithms(Baekjoon)/Q 9012.py
@@ -1,9 +1,27 @@
-# 2021.02.22. Baekjoon algorithm problem #9012
+# 2021.02.23. Baekjoon algorithm problem #9012
 # Parenthesis
 
 import sys
 
+table = {')' : '('}
+
 N = int(input())
-for _ in range(N):
-    stack = list(map(str,sys.stdin.readline()))
+answer = [True]*N
+
+for i in range(N):
+    stack = []
+    sequence = list(map(str,sys.stdin.readline().rstrip()))
+    for char in sequence:
+        # in case of (
+        if char not in table:
+            stack.append(char)
+        # in case of ), Exception processing(No stack)
+        elif not stack or table[char] != stack.pop() :
+            answer[i] = False
+    # print
+    if answer[i] == True and len(stack)==0:
+        print('YES')
+    else:
+        print('NO')
+
 
