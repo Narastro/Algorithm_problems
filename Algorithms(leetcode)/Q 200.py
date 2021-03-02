c0a24810f21f564245d3cef425a8bb6760e1f6f0
Narastro <78056880+Narastro@users.noreply.github.com>
2021-03-02 10:44:58 +0900

    * Number of Islands

---
+++ Algorithms(leetcode)/Q 200.py
@@ -0,0 +1,26 @@
+# 2021.03.02. Leetcode algorithm problem #200
+# Number of Islands
+
+def Num_is_Islands(self, grid:list[list[str]])->int:
+    def dfs(i,j):
+        # terminate if it is no longer lands
+        if i < 0 or i >= len(grid) or \
+            j < 0 or j >= len(grid[0] or \
+            grid[i][j] != '1' :
+            return
+        grid[i][j] = 0
+
+        dfs(i+1,j)
+        dfs(i-1,j)
+        dfs(i , j+1)
+        dfs(i, j-1)
+
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == '1':
+                dfs(i,j)
+                # after searching, count+1
+                count += 1
+    return count
+
