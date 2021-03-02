15e186214fe03d27dbe64ec63a8458a748daf25c
Narastro <78056880+Narastro@users.noreply.github.com>
2021-03-02 10:48:02 +0900

    * Number of Islands

--- Algorithms(leetcode)/Q 200.py
+++ Algorithms(leetcode)/Q 200.py
@@ -5,8 +5,8 @@ def Num_is_Islands(self, grid:list[list[str]])->int:
     def dfs(i,j):
         # terminate if it is no longer lands
         if i < 0 or i >= len(grid) or \
-            j < 0 or j >= len(grid[0] or \
-            grid[i][j] != '1' :
+                j < 0 or j >= len(grid[0]) or \
+                grid[i][j] != '1' :
             return
         grid[i][j] = 0
 
