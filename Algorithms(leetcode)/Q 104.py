e640cb2896a07d018e1c999863dac6715564d6df
Narastro <78056880+Narastro@users.noreply.github.com>
2021-03-16 22:16:11 +0900

    Maximum Depth of Binary Tree

---
+++ Algorithms(leetcode)/Q 104.py
@@ -0,0 +1,20 @@
+# 2021.03.08. Leetcode algorithm problem #104
+# Maximum Depth of Binary Tree
+
+import collections
+
+def maxDepth(root:TreeNode) -> int:
+    if root in None:
+        return 0
+    queue = collections.deque([root])
+    depth = 0
+
+    while queue:
+        depth += 1
+        for _ in range(len(queue)):
+            cur_root = queue.popleft()
+            if cur_root,left:
+                queue.append(cur_root.left)
+            if cur_root.right:
+                queue.append(cur_root.right)
+    return depth
