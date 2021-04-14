2edfc749689d7176619c3352b0d9a298625efdfd
Narastro <78056880+Narastro@users.noreply.github.com>
2021-04-14 21:46:13 +0900

    Distance_visit.py

---
+++ Algorithms (Programmers)/Summer-Winter Coding/2018_Distance_visit.py
@@ -0,0 +1,26 @@
+# 2021.04.14. Summer-Winter coding(2018)
+# Distance visit
+
+def solution(dirs):
+    direction = set()
+    dx, dy = 0, 0
+    # 1. for each command
+    for command in dirs:
+        x, y = dx, dy
+        if command == 'U' and dy < 5:
+            dy += 1
+        elif command == 'D' and dy > -5:
+            dy -= 1
+        elif command == 'L' and dx > -5:
+            dx -= 1
+        elif command == 'R' and dx < 5:
+            dx += 1
+        # 2. out of map
+        else:
+            continue
+        # 3. both sides
+        direction.add((x, y, dx, dy))
+        direction.add((dx, dy, x, y))
+    return len(direction)/2
+    
+print(solution())
