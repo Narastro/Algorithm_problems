c9bd8f986ba7a00feae1e43de4d01fb2f1b21e20
Narastro <78056880+Narastro@users.noreply.github.com>
2021-04-17 22:52:24 +0900

    all_zero.py

--- Algorithms (Programmers)/Monthly-code-challenge/all-0.py
+++ Algorithms (Programmers)/Monthly-code-challenge/all-0.py
@@ -1,6 +1,29 @@
 # 2021.04.16. 2021 Programmers monthly code challenge season2
 # Making all 0
 
+from collections import defaultdict
 def solution(a, edges):
+    if sum(a) != 0:
+        return -1
+
+    tree = defaultdict(list)
+    
+    for edge for edges:
+        tree[edge[0]].append(edge[1])
+        tree[edge[1]].append(edge[0])
+
+    leaves = []
+    for node in tree:
+        if len(node)==1:
+            leaves.append(node)
+    n = len(a)
+    while n > 1:
+        n -= len(leaves)
+        new_leaves = []
+        for leaf in leaves:
+            neighbor = tree[leaf].pop()
+
+        leaves = new_leaves
+
     answer = -2
     return answer
