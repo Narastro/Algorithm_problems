5332a28ad5de20767aa24ef46be13921ea5f567e
Narastro <78056880+Narastro@users.noreply.github.com>
2021-04-28 19:05:14 +0900

    Largest Number - py

---
+++ Algorithms(leetcode)/Q 179.py
@@ -0,0 +1,17 @@
+# 2021.04.28. Leetcode algorithm problem #179
+# Largest Number
+
+class solution:
+    @staticmethod
+    def swap(n1,n2):
+        return str(n1)+str(n2) < str(n2) + str(n1)
+    
+    def largestNumber(self,nums):
+        i = 1
+        while i < len(nums):
+            j = i
+            while j>0 and self.swap(nums[j-1],nums[j]):
+                num[j], nums[j-1] = nums[j-1],nums[j]
+                j-=1
+            i += 1
+        return str(int(''.join(map(str,nums))))
