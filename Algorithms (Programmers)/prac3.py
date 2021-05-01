b9c00f2ea25159b956bdbb40a92a72754a68c53e
Narastro <78056880+Narastro@users.noreply.github.com>
2021-05-01 18:00:51 +0900

    my answer - py

---
+++ Algorithms (Programmers)/prac3.py
@@ -0,0 +1,24 @@
+def solution(ads):
+    answer = 0
+    time = 0
+
+    ads.sort(key=lambda x: (x[0], -x[1]), reverse=True)
+
+    first = ads.pop()
+    time = first[0] + 5
+
+    wait = []
+    while ads or wait:
+        while ads and ads[-1][0] <= time:
+            wait.append(ads.pop())
+        if not wait:
+            time = ads.pop()[0] + 5
+        else:
+            wait.sort(key=lambda x: (time-x[0])*x[1], reverse=True)
+            w = wait.pop()
+            answer += (time-w[0])*w[1]
+            time += 5
+
+    return answer
+
+print(solution([[0, 1], [0, 2], [6, 3], [8, 4]]))
