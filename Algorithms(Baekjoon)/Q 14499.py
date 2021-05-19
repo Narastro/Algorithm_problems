217dfa87e9247a34c209f02c5da29dac86598bf7
Narastro <78056880+Narastro@users.noreply.github.com>
2021-05-19 11:34:31 +0900

    Rolling a dice - py

--- Algorithms(Baekjoon)/Q 14499.py
+++ Algorithms(Baekjoon)/Q 14499.py
@@ -4,9 +4,44 @@
 import sys
 input = sys.stdin.readline
 
-def solution():
+
+def move(direction):
+    if direction == 1:
+        dice[1], dice[3], dice[4], dice[6] = dice[3], dice[6], dice[1], dice[4]
+    elif direction == 2:
+        dice[1], dice[3], dice[4], dice[6] = dice[4], dice[1], dice[6], dice[3]
+    elif direction == 3:
+        dice[1], dice[2], dice[5], dice[6] = dice[2], dice[6], dice[1], dice[5]
+    elif direction == 4:
+        dice[1], dice[2], dice[5], dice[6] = dice[5], dice[1], dice[6], dice[2]
 
 
-N,M,x,y,K = map(int,input())
+def dire(direction):
+    if direction == 1:
+        return 0, 1
+    elif direction == 2:
+        return 0, -1
+    elif direction == 3:
+        return -1, 0
+    elif direction == 4:
+        return 1, 0
 
-for i in range(N):
+
+n, m, x, y, k = map(int, input().split())
+s = []
+for i in range(n):
+    s.append(list(map(int, input().split())))
+com = list(map(int, input().split()))
+dice = [0, 0, 0, 0, 0, 0, 0]
+for i in com:
+    a, b = dire(i)
+    if 0 <= x + a < n and 0 <= y + b < m:
+        x += a
+        y += b
+        move(i)
+        if s[x][y] != 0:
+            dice[1] = s[x][y]
+            s[x][y] = 0
+        else:
+            s[x][y] = dice[1]
+        print(dice[6])
