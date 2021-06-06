596c5577d29cbbe09b96a1035437a9db0e7e4ece
Narastro <advice02@naver.com>
2021-06-06 14:49:03 +0900

    modified

--- Algorithms (Programmers)/0605.py
+++ Algorithms (Programmers)/0605.py
@@ -19,26 +19,26 @@ print(solution('12390111'))
 
 #####2번
 
-# def solution(endingTime, jobs):
-#     answer = []
-#     cur_time = 0
-#     for job in jobs:
-#         if job[1]> cur_time:
-#             cur_time = job[1]+job[3]
-#         else:
-#             cur_time += job[3]
+def solution(endingTime, jobs):
+    answer = []
+    cur_time = 0
+    for job in jobs:
+        if job[1]> cur_time:
+            cur_time = job[1]+job[3]
+        else:
+            cur_time += job[3]
         
-#         if cur_time <= job[2] and cur_time<=endingTime:
-#             answer.append(job[0])
+        if cur_time <= job[2] and cur_time<=endingTime:
+            answer.append(job[0])
 
-#     return answer
+    return answer
 
-# print(solution(30,[[1, 10, 20, 6], [2, 12, 20, 8], [3, 20, 30, 2], [4, 25, 40, 10]]))
+print(solution(30,[[1, 10, 20, 6], [2, 12, 20, 8], [3, 20, 30, 2], [4, 25, 40, 10]]))
 
 
 
-##### 3번
+#### 3번
 
-# def solution(n, data, limit):
-#     answer = []
-#     return answer
+def solution(n, data, limit):
+    answer = []
+    return answer
