77d35ee8e2a06a9ee83240ded497c462887234f2
Narastro <advice02@naver.com>
2021-06-12 17:45:42 +0900

    greedy - 성냥개비

--- Algorithms(Baekjoon)/Q 3687.py
+++ Algorithms(Baekjoon)/Q 3687.py
@@ -2,20 +2,28 @@
 # 성냥개비
 
 import sys
+from itertools import product
+
 table = {
-    2:[1],
-    3:[7],
-    4:[4],
-    5:[2,5],
-    6:[0,6,9],
-    7:8
+    2:'1',
+    3:'7',
+    4:'4',
+    5:'2',
+    6:'0',
+    7:'8'
 }
 
-def min_sol(N,table):
-    pos = N//7
-    answer = ''
-    for i in range(pos):
-        for k in [2,5,4,6,3,7]:
+def min_sol(N):
+    ret = [8 for i in range((N+6)//7)]
+    if N%7==1: ret[0]=1;ret[1]=0
+    if N%7==2: ret[0]=1
+    if N%7==3: ret[0]=2;ret[1]=0;ret[2]=0
+    if N%7==4: ret[0]=2;ret[1]=0
+    if N%7==5: ret[0]=2
+    if N%7==6: ret[0]=6
+    return ret
+
+
             
     
 def max_sol(N):
@@ -25,8 +33,13 @@ def max_sol(N):
         return '7'+'1'*(N//2-1)
 
 
+gridy = [0,0,1,7,4,2,6,8,10,18,22]
 
 T = int(input())
 for _ in range(T):
     N = int(input())
-    min_val = sys.maxsize
+    if N<11:
+        print(gridy[N],max_sol(N))
+    else:
+        print(*min_sol(N),sep='',end=" ")
+        print(max_sol(N))
