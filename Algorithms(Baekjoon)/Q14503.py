9173e883e0317a84961b446fc6de1ff2a60fd365
Narastro <advice02@naver.com>
2021-08-20 21:35:09 +0900

    백준 14503 - 로봇청소기 (구현)

--- Algorithms(Baekjoon)/Q14503.py
+++ Algorithms(Baekjoon)/Q14503.py
@@ -16,57 +16,71 @@ r, c, d = map(int, input().split())
 location = [list(map(int, input().split())) for _ in range(N)]
 
 
-def change_dir(d):
+def left_check(x, y, d):
+    nd = (d+3) % 4
+    nx = x + dx[nd]
+    ny = y + dy[nd]
+    if 0 <= nx < M and 0 <= ny < N and location[ny][nx] == 0:
+        return True
+    return False
+
+
+def turn_left(d):
     return (d+3) % 4
 
 
-def already_clean(x, y):
-    cleaned = 0
+def four_way_already(x, y):
+    already = 0
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if nx < 0 or nx >= M or ny < 0 or ny >= N or location[ny][nx] != 0:
-            cleaned += 1
-    return cleaned == 4
+            already += 1
+    return already == 4
 
 
-def front(x, y, d):
-    nx = x + dx[d]
-    ny = y + dy[d]
-    if 0 <= nx < M and 0 <= ny < N and location[ny][nx] == 0:
-        return nx, ny
-
-    else:
-        return False
-
-
-def reverse(x, y, d):
+def backward_pos(x, y, d):
     nd = (d+2) % 4
     nx = x + dx[nd]
     ny = y + dy[nd]
-
     if nx < 0 or nx >= M or ny < 0 or ny >= N or location[ny][nx] == 1:
         return False
+    return True
 
-    for _ in range(2):
-        d = change_dir(d)
-    return d
+
+def turn_back(d):
+    return (d+2) % 4
+
+
+def go_straight(x, y, d):
+    nx = x + dx[d]
+    ny = y + dy[d]
+    return nx, ny
+
+
+def printFun():
+    for j in range(N):
+        print(location[j])
 
 
 while True:
-    print(r, c)
+    # print()
+    # printFun()
     location[r][c] = 2
-    if already_clean(c, r):
-        if not reverse(c, r, d):
-            break
-        d = reverse(c, r, d)
-        if not front(c, r, d):
-            c, r = front(c, r, d)
-    else:
-        d = change_dir(d)
-        if not front(c, r, d):
+    if four_way_already(c, r):
+        if backward_pos(c, r, d):
+            c, r = go_straight(c, r, turn_back(d))
             continue
-        c, r = front(c, r, d)
+        else:
+            break
+
+    if left_check(c, r, d):
+        d = turn_left(d)
+        c, r = go_straight(c, r, d)
+        continue
+    else:
+        d = turn_left(d)
+        continue
 
 answer = 0
 for j in range(N):
