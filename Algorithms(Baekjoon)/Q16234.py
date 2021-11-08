3cef239067be22b88e9b830861dab318421df58e
Narastro <advice02@gmail.com>
2021-11-08 23:32:19 +0900

    Solving: 인구 이동 푸는 중...
    졸리네요... 다음에 계속...

---
+++ Algorithms(Baekjoon)/Q16234.py
@@ -0,0 +1,9 @@
+# 2021.11.08. Baekjoon algorithm problem #16234
+# 인구 이동
+
+N, L, R = map(int, input().split())
+table = [list(map(int, input().split())) for _ in range(N)]
+
+
+def find_union(node):
+    return
