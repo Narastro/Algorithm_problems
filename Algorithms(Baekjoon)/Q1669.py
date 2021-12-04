5c778bf65df402df9c69e1617862e9600558256c
Narastro <advice02@gmail.com>
2021-12-04 23:24:33 +0900

    Solved: 수학 - 멍멍이 쓰다듬기(백준)

--- Algorithms(Baekjoon)/Q1669.py
+++ Algorithms(Baekjoon)/Q1669.py
@@ -6,3 +6,12 @@ N = Y-X
 if N == 0:
     print(0)
 else:
+    n = int(N**0.5)
+    if n**2 == N:
+        print(2*n-1)
+    else:
+        k = N - n**2
+        if k <= n:
+            print(2*n)
+        else:
+            print(2*n+1)
