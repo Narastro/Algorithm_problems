0446c2294e0f29b5fca56142ec97ea2c5eeb8a19
Narastro <advice02@naver.com>
2021-12-08 21:42:32 +0900

    Solved: 완전 탐색

---
+++ programmers/high-score/소수찾기.js
@@ -0,0 +1,39 @@
+const isPrimeNumber = (num) => {
+  if (num < 2) return false;
+  if (num === 2) return true;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const Permutations = (arr, selectNumber) => {
+  const results = [];
+  if (selectNumber === 1) return arr.map((value) => [value]);
+
+  arr.forEach((fixed, index, origin) => {
+    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+    const permutations = Permutations(rest, selectNumber - 1);
+    const attached = permutations.map((permutation) => [fixed, ...permutation]);
+    results.push(...attached);
+  });
+
+  return results;
+};
+
+function solution(numbers) {
+  let answer = 0;
+  const numberArr = new Set();
+  for (let i = 1; i <= numbers.length; i++) {
+    const permNumArr = Permutations(numbers.split(""), i);
+    for (let perArr of permNumArr) {
+      numberArr.add(Number(perArr.join("")));
+    }
+  }
+  for (let num of numberArr) {
+    if (isPrimeNumber(num)) answer += 1;
+  }
+  return answer;
+}
+
+console.log(Math.sqrt(4));
