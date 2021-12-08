3892022ca7bae0dc16967ff4275d718875288b4a
Narastro <advice02@naver.com>
2021-12-08 22:15:34 +0900

    Retry: 아기 상어

---
+++ baekjoon/16236.js
@@ -0,0 +1,19 @@
+/*
+2021.08.19. 2021 Baekjoon algorithm problem #16236
+아기 상어
+*/
+
+/*
+<풀이 아이디어>
+1) 
+*/
+
+const fs = require("fs");
+const filePath = process.env.USER === "narastro" ? "./input.txt" : "/dev/stdin";
+const input = fs.readFileSync(filePath).toString().split(`\n`);
+
+function solution(input) {
+  const edible = (x, y) => {};
+
+  const findFish = () => {};
+}
