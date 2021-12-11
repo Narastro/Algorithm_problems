db3ab896041442341ca76aaa440a0340896e5dd6
Narastro <advice02@naver.com>
2021-12-11 22:59:49 +0900

    Solving: 추석 트래픽 - 아직 푸는 중 ㅠ

--- programmers/Kakao problem/추석트래픽.js
+++ programmers/Kakao problem/추석트래픽.js
@@ -2,4 +2,31 @@
  * 추석 트래픽
  */
 
-const solution = (lines) => {};
+const timeToMillisec = (str) => {
+  const strArr = str.split(":");
+  const hours = Number(strArr[0]);
+  const minutes = Number(strArr[1]);
+  const milliseconds = Number(strArr[2]) * 1000;
+  const total = milliseconds + minutes * 60 * 1000 + hours * 60 * 60 * 1000;
+  return total;
+};
+
+const findStartTime = (endTime, time) =>
+  endTime - Number(time.split("s")[0]) * 1000;
+
+const solution = (lines) => {
+  const logs = [];
+  const timeInfo = [];
+  let maxCount = 0;
+
+  lines.forEach((line) => {
+    const endTime = timeToMillisec(line.split(" ")[1]);
+    const startTime = findStartTime(endTime, line.split(" ")[2]);
+    timeInfo.push({ startTime, endTime });
+    logs.push(startTime, endTime);
+  });
+
+  logs.sort();
+  logs.forEach((log) => {});
+  return logs;
+};
