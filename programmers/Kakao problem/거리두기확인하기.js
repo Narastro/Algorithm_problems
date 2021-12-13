96a3898750e617cfae615c1b75302b565eeebf7d
Narastro <advice02@naver.com>
2021-12-13 15:38:12 +0900

    Solved: 거리두기 확인하기 (카카오 2021 인턴십 기출)

--- programmers/Kakao problem/거리두기확인하기.js
+++ programmers/Kakao problem/거리두기확인하기.js
@@ -2,75 +2,39 @@
  * 거리두기 확인하기
  */
 
-class NodeQueue {
-  constructor(value) {
-    this.next = null;
-    this.value = value;
-  }
-}
-
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-
-  push(value) {
-    const nodeQueue = new NodeQueue(value);
-    if (!this.size) this.head = nodeQueue;
-    else this.tail.next = nodeQueue;
-    this.tail = nodeQueue;
-    this.size++;
-  }
-
-  popleft() {
-    if (!this.size) return null;
-    const value = this.head.value;
-    this.head = this.head.next;
-    this.size--;
-    if (!this.size) this.tail = null;
-    return value;
-  }
-
-  isEmpty() {
-    return this.size === 0;
-  }
-}
-
 const solution = (places) => {
   const N = 5;
   const dx = [0, 1, -1, 0];
   const dy = [1, 0, 0, -1];
 
-  const isSafe = (place, row, col) => {
-    const visit = new Array(N);
-    for (let i = 0; i < N; i++) {
-      visit[i] = new Array(N).fill(-1);
-    }
-    const queue = new Queue();
-    queue.push({ row, col });
-    visit[row][col] = 0;
-    while (!queue.isEmpty()) {
-      const node = queue.popleft();
-      const dist = visit[node.row][node.col];
+  const isSafe = (value, place, row, col) => {
+    if (value === "P") {
       for (let i = 0; i < 4; i++) {
-        const nRow = node.row + dy[i];
-        const nCol = node.col + dx[i];
-        const newNode = { row: nRow, col: nCol };
+        const nRow = row + dy[i];
+        const nCol = col + dx[i];
         if (
-          0 < nRow &&
-          0 < nCol &&
+          0 <= nRow &&
+          0 <= nCol &&
           nRow < N &&
           nCol < N &&
-          visit[nRow][nCol] === -1 &&
-          place[nRow][nCol] !== "X"
+          place[nRow][nCol] === "P"
+        )
+          return false;
+      }
+    } else if (value === "O") {
+      let cnt = 0;
+      for (let i = 0; i < 4; i++) {
+        const nRow = row + dy[i];
+        const nCol = col + dx[i];
+        if (
+          0 <= nRow &&
+          0 <= nCol &&
+          nRow < N &&
+          nCol < N &&
+          place[nRow][nCol] === "P"
         ) {
-          if (dist < 2 && place[nRow][nCol] === "P") {
-            return false;
-          }
-          queue.push(newNode);
-          visit[nRow][nCol] = dist + 1;
+          cnt += 1;
+          if (cnt > 1) return false;
         }
       }
     }
@@ -81,7 +45,7 @@ const solution = (places) => {
     let answer = true;
     place.forEach((line, row) => {
       line.split("").forEach((value, col) => {
-        if (value === "P" && !isSafe(place, row, col)) answer = false;
+        if (value !== "X" && !isSafe(value, place, row, col)) answer = false;
       });
     });
     if (answer) return 1;
@@ -89,12 +53,4 @@ const solution = (places) => {
   });
 };
 
-console.log(
-  solution([
-    ["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
-    ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"],
-    ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"],
-    ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"],
-    ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"],
-  ])
-);
+console.log(solution([["OP", "PX"]]));
