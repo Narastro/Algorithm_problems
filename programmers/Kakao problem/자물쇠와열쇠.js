28b801c82cf6df1afaf9e8c5b3bd6506f38a70bc
Narastro <advice02@naver.com>
2021-12-15 23:17:12 +0900

    Ready: 자물쇠와 열쇠 풀이 아이디어만 생각

---
+++ programmers/Kakao problem/자물쇠와열쇠.js
@@ -0,0 +1,13 @@
+/* 2021.12.14. 2020 KAKAO BLIND RECRUITMENT
+ * 자물쇠와 열쇠
+ */
+
+const moveKey = (x, y, N) => {
+  return;
+};
+
+const rotate = (x, y, N) => {
+  return { x: N - y - 1, y: N - x - 1 };
+};
+
+const solution = (key, lock) => {};
