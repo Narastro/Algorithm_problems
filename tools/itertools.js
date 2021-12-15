8f9a14c0961c30e99f3631c2cfb7e52ce2366bd5
Narastro <advice02@naver.com>
2021-12-15 20:19:19 +0900

    Fix: 순열, 조합 함수 주석 제거

--- tools/itertools.js
+++ tools/itertools.js
@@ -3,9 +3,9 @@ function Combinations(arr, selectNumber) {
   if (selectNumber === 1) return arr.map((value) => [value]);
 
   arr.forEach((fixed, index, origin) => {
-    const rest = origin.slice(index + 1); // 현재 문자 이후 배열을 자름
-    const combinations = Combinations(rest, selectNumber - 1); // 그 배열에 대해 재귀으로 조합을 구함
-    const attached = combinations.map((combination) => [fixed, ...combination]); // 그 조합에 현재 문자를 추가함
+    const rest = origin.slice(index + 1);
+    const combinations = Combinations(rest, selectNumber - 1);
+    const attached = combinations.map((combination) => [fixed, ...combination]);
     results.push(...attached);
   });
 
@@ -17,7 +17,7 @@ function Permutations(arr, selectNumber) {
   if (selectNumber === 1) return arr.map((value) => [value]);
 
   arr.forEach((fixed, index, origin) => {
-    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)]; // 조합과의 차이점은 현재 문자를 제외한 모든 배열을 자름
+    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
     const permutations = Permutations(rest, selectNumber - 1);
     const attached = permutations.map((permutation) => [fixed, ...permutation]);
     results.push(...attached);
