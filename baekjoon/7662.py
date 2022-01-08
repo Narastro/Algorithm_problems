179e2088f05ea6fcf764c731b28262514cf72d4b
Narastro <advice02@naver.com>
2022-01-08 12:19:19 +0900

    Solved: 이중 우선순위 큐(백준) - 우선순위큐

---
+++ baekjoon/7662.py
@@ -0,0 +1,45 @@
+# 2021.01.08. Baekjoon algorithm problem #7662
+# 이중 우선순위 큐
+
+
+import sys
+import heapq
+input = sys.stdin.readline
+
+
+T = int(input())
+for _ in range(T):
+    max_heap, min_heap = [], []
+    visit = [False] * 1000001
+
+    input_num = int(input())
+
+    for index in range(input_num):
+        order = input().rsplit()
+        if order[0] == 'I':
+            heapq.heappush(min_heap, (int(order[1]), index))
+            heapq.heappush(max_heap, (int(order[1]) * -1, index))
+            visit[index] = True
+        elif order[0] == 'D':
+            if order[1] == '-1':
+                while min_heap and not visit[min_heap[0][1]]:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    visit[min_heap[0][1]] = False
+                    heapq.heappop(min_heap)
+            elif order[1] == '1':
+                while max_heap and not visit[max_heap[0][1]]:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    visit[max_heap[0][1]] = False
+                    heapq.heappop(max_heap)
+
+    while min_heap and not visit[min_heap[0][1]]:
+        heapq.heappop(min_heap)
+    while max_heap and not visit[max_heap[0][1]]:
+        heapq.heappop(max_heap)
+
+    if min_heap and max_heap:
+        print(-max_heap[0][0], min_heap[0][0])
+    else:
+        print('EMPTY')
