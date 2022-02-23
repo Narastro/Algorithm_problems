4351644c92631833548ee818272f6f81f5638a53
Dale <dale.vand@kakaoenterprise.com>
2022-02-23 23:55:32 +0900

    Docs: 리드미 업데이트

--- programmers/high-score/hIndex.js
+++ programmers/high-score/hIndex.js
@@ -1,11 +1,16 @@
-/* 2021.05.04. 2021 Programmers High score kit
+/* 2021.06.27. 2021 Programmers High score kit
  * H index
  */
 
 function solution(citations) {
-    let answer = 0;
-    
-    return answer;
+  let answer = 0;
+  citations.sort((a, b) => b - a);
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] >= i + 1) {
+      answer = i + 1;
+    }
+  }
+  return answer;
 }
 
-console.log(solution([3,0,6,1,5]))
+console.log(solution([3, 0, 6, 1, 5]));
