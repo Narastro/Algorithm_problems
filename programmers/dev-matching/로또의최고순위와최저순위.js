d998d24785d0b3e7b421894201754d1f83217d22
Dale <dale.vand@kakaoenterprise.com>
2022-04-04 23:01:13 +0900

    Solved: 로또의 최고 순위와 최저 순위(프로그래머스)

---
+++ programmers/dev-matching/로또의최고순위와최저순위.js
@@ -0,0 +1,18 @@
+/* 2022.4.4. 2021 Dev-Matching: 웹 백엔드
+ * 로또의 최고 순위와 최저 순위
+ */
+
+function solution(lottos, win_nums) {
+  const answer = [7, 7];
+  lottos.forEach((num) => {
+    if (win_nums.includes(num)) {
+      answer[0] -= 1;
+      answer[1] -= 1;
+    } else if (num === 0) {
+      answer[0] -= 1;
+    }
+  });
+  if (answer[0] === 7) answer[0] = 6;
+  if (answer[1] === 7) answer[1] = 6;
+  return answer;
+}
